25d0f36cc54afc110d89259dd55af8aa744254e6
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-15 12:10:39 +1000

    Working on presentation logic.

---
+++ Ex1/BasicCalculator.js
@@ -0,0 +1,8 @@
+﻿function BasicCalculator($scope, $log) {
+    $scope.addend1 = 5;
+    $scope.addend2 = 10;
+
+    $scope.add = function () {
+        $scope.sum = parseFloat($scope.addend1) + parseFloat($scope.addend2);
+    };
+}
---
+++ Presentation.js
@@ -0,0 +1,46 @@
+﻿// Get the angular module (this application)
+var module = angular.module(
+    "presentation",               // Module name
+    []                            // No dependencies on other modules
+);
+
+// Create a "slides" service
+module.factory('slides', function() {
+    // Add or remove slides by adding them to the array below 
+    var slidesService = [
+        {
+            title: "Basic Calculator (AngularJS)",
+            url: "Ex1/BasicCalculator.html"
+        },
+        {
+            title: "Basic Calculator (Web Forms)",
+            url: "Ex1/BasicCalculatorWebForm.aspx"
+        }
+    ];
+
+    return slidesService;
+});
+
+// Configure routes
+module.config(function ($routeProvider, slides) {
+    // Add each slide
+    for (var i = 0; i < slides.length; i++) {
+        $routeProvider.when("/slides/:slideIndex", {
+            templateUrl: slides[i].url,
+            controller: PresentationController
+        });
+    }
+        
+    // Add a default route
+    $routeProvider.otherwise({ redirectTo: "/slides/1" });
+});
+
+// The controller
+
+function PresentationController($scope, $routeParams, slides) {
+    if ($routeParams.slideIndex == undefined) {
+        $scope.title = "Presentation Title";
+    } else {
+        $scope.title = slides[$routeParams.slideIndex].title;
+    }
+}
