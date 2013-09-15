3aa6a35d27ded9ca54d86acf32bc19763061e8cd
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-15 22:53:46 +1000

    About to change view to include.

--- Presentation.js
+++ Presentation.js
@@ -14,8 +14,8 @@ module.constant('slides', [
         url: "Ex1/BasicCalculator.html"
     },
     {
-        title: "Basic Calculator (Web Forms)",
-        url: "Ex2/BasicCalculatorWebForm.aspx"
+        title: "Page 2",
+        url: "Ex1/BasicCalculator.html"
     }
 ]);
 
@@ -29,21 +29,67 @@ module.constant('slides', [
 module.config(function ($routeProvider, slides) {
     // Add each slide
     for (var i = 0; i < slides.length; i++) {
-        $routeProvider.when("/slides/:slideIndex", {
-            templateUrl: slides[i].url,
+        $routeProvider.when("/slides/:slideIndex",
+        {
+            templateUrl: "Slide.html",
             controller: PresentationController
         });
     }
         
     // Add a default route
-    $routeProvider.otherwise({ redirectTo: "/slides/1" });
+    $routeProvider.otherwise(
+    {
+        redirectTo: "/slides/1"
+    });
 });
 
 // The controller
-function PresentationController($scope, $routeParams, slides) {
-    if ($routeParams.slideIndex == undefined) {
-        $scope.$parent.title = "Presentation Title";
+function PresentationController($scope, $routeParams, $location, slides) {
+    // Use an object so the fields are synchronized with the parent scope.
+    $scope.presentation = {};
+    
+    // Set the fields
+    if (slides == undefined || slides.length == 0) {      // No slides
+        $scope.presentation.currentSlide = 1;
+        $scope.presentation.slideCount = 0;
+        $scope.presentation.title = "(No Slides)";
+    } else if ($routeParams.slideIndex == undefined       // Check whether slideIndex is included in the URL
+            || /\D/.test($routeParams.slideIndex)         // Check whether it contanis non-digits
+            || parseInt($routeParams.slideIndex) < 1
+            || parseInt($routeParams.slideIndex) > slides.length) {      
+        $scope.presentation.currentSlide = 1;
+        $scope.presentation.slideCount = slides.length;
+        $scope.presentation.title = slides[0].title;
     } else {
-        $scope.$parent.title = slides[parseInt($routeParams.slideIndex) - 1].title;
+        $scope.presentation.currentSlide = parseInt($routeParams.slideIndex);
+        $scope.presentation.slideCount = slides.length;
+        $scope.presentation.title = slides[$scope.presentation.currentSlide - 1].title;
     }
+
+    // True if the current slide is not the first slide in the presentation.
+    $scope.canGoBack = function() {
+        return $scope.presentation.currentSlide > 1;
+    };
+    
+    // True if the current slide is not the last slide in the presentation.
+    $scope.canGoForward = function () {
+        return $scope.presentation.currentSlide < slides.length;
+    };
+
+    // Go back one slide
+    $scope.goBack = function () {
+        if (!$scope.canGoBack()) {
+            throw new Error("Cannot go back");
+        }
+        $location.path("/slides/" + ($scope.presentation.currentSlide - 1));
+    };
+
+    // Go forward one slide
+    $scope.goForward = function () {
+        if (!$scope.canGoForward()) {
+            throw new Error( "Cannot go forward");
+        }   
+        $location.path("/slides/" + ($scope.presentation.currentSlide + 1));
+    };
+
 }
