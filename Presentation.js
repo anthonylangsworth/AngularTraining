7f1fb5e3f119d7e1d54dbf97cb6a802fb5f43a8b
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-15 14:13:32 +1000

    Moved to a constant. Adding comments.

--- Presentation.js
+++ Presentation.js
@@ -4,24 +4,46 @@ var module = angular.module(
     []                            // No dependencies on other modules
 );
 
-// Create a "slides" service
-module.factory('slides', function() {
-    // Add or remove slides by adding them to the array below 
-    var slidesService = [
-        {
-            title: "Basic Calculator (AngularJS)",
-            url: "Ex1/BasicCalculator.html"
-        },
-        {
-            title: "Basic Calculator (Web Forms)",
-            url: "Ex1/BasicCalculatorWebForm.aspx"
-        }
-    ];
+// I could create  a "slides" service but zall the service would contain is
+// data so that is what the $rootScope is for. See "$rootScope exists, 
+// but it can be used for evil" at http://docs.angularjs.org/misc/faq.
+//module.factory('slides', function() {
+//    // Add or remove slides by adding them to the array below 
+//    var slidesService = [
+//        {
+//            title: "Basic Calculator (AngularJS)",
+//            url: "Ex1/BasicCalculator.html"
+//        },
+//        {
+//            title: "Basic Calculator (Web Forms)",
+//            url: "Ex1/BasicCalculatorWebForm.aspx"
+//        }
+//    ];
 
-    return slidesService;
-});
+//    return slidesService;
+//});
+
+// Add or remove slides by adding them to the array below. 
+// Use this rather than a global variable to allow easier
+// testing.
+module.constant('slides', [
+    {
+        title: "Basic Calculator (AngularJS)",
+        url: "Ex1/BasicCalculator.html"
+    },
+    {
+        title: "Basic Calculator (Web Forms)",
+        url: "Ex2/BasicCalculatorWebForm.aspx"
+    }
+]);
 
 // Configure routes
+//
+// Note that 'config' occurs early in the initialization process
+// and most services are not available. Constants, such as 'slides',
+// and dependencies on other modules, such as '$routeProvider', are
+// the exceptions. See http://docs.angularjs.org/guide/module for
+// more information.
 module.config(function ($routeProvider, slides) {
     // Add each slide
     for (var i = 0; i < slides.length; i++) {
@@ -36,7 +58,6 @@ module.config(function ($routeProvider, slides) {
 });
 
 // The controller
-
 function PresentationController($scope, $routeParams, slides) {
     if ($routeParams.slideIndex == undefined) {
         $scope.title = "Presentation Title";
