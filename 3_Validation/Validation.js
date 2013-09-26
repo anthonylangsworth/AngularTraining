e5d8f9fb4f957763e442cae6a03a8967f76044aa
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-27 00:35:16 +1000

    Got blur validation working.

--- 3_Validation/Validation.js
+++ 3_Validation/Validation.js
@@ -1,19 +1,21 @@
 ﻿var module = angular.module(
-        "validation.example",  // Name
-        []);                   // Dependencies
+        "validationExample",  // Name
+        []);                  // Dependencies
 
 var regExp = /^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/;
 
-module.directive('cbaStaffNumber', function () {                // Called once when the first instance of the directive is parsed.
-                                                                // Injection is possible, e.g. pass $http, $timeout, etc
-    return function(scope, element, attributes, controller) {   // No injection
-        debugger;
-        if (regExp.test(element.value)) {
-            controller.$setValidity('cbaStaffNumber', true);
-            return element;
-        } else {
-            controller.$setValidity('cbaStaffNumber', true);
-            return undefined;
+module.directive('cbaStaffNumber', function () {                  // Called once when the first instance of the directive is parsed.
+                                                                  // Injection is possible, e.g. pass $http, $timeout, etc
+    return {
+        require: 'ngModel',                                       // Only applied if the "ng-model" attribute/directive is applied and
+                                                                  // inject its controller as the fourth argument to the link function.
+        link: function (scope, element, attributes, controller) { // No injection. Called once, not once each time the value is changed.
+            element.bind("blur", function() {
+                controller.$setValidity(
+                    'cbaStaffNumber',                             // Value to set on $error object
+                    regExp.test(element[0].value));               // True/false state
+                scope.$apply();
+            });
         }
     };
 });
