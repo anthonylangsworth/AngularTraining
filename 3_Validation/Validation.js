507cffe900fcc2ee839b673ccc11987f7c3738b8
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-26 23:41:11 +1000

    Working on validation examples.

---
+++ 3_Validation/Validation.js
@@ -0,0 +1,24 @@
+﻿var module = angular.module(
+        "validation.example",  // Name
+        []);                   // Dependencies
+
+var regExp = /^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/;
+
+module.directive('cbaStaffNumber', function () {                // Called once when the first instance of the directive is parsed.
+                                                                // Injection is possible, e.g. pass $http, $timeout, etc
+    return function(scope, element, attributes, controller) {   // No injection
+        debugger;
+        if (regExp.test(element.value)) {
+            controller.$setValidity('cbaStaffNumber', true);
+            return element;
+        } else {
+            controller.$setValidity('cbaStaffNumber', true);
+            return undefined;
+        }
+    };
+});
+
+// Controller
+function ValidationController($scope) {
+    // Do nothing
+}
--- Ex2/Validation.js
+++
@@ -1,5 +0,0 @@
-﻿angular
-    .module()
-    .directive('cbaStaffNumber', function () {
-    
-})
