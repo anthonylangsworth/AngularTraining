edbb6851622dd799ac1bb3f94314035e0d8e8bbe
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-27 00:45:13 +1000

    Cleaned up example.

--- 3_Validation/Validation.js
+++ 3_Validation/Validation.js
@@ -1,26 +1,22 @@
 ﻿var module = angular.module(
         "validationExample",  // Name
-        []);                  // Dependencies
+        []);                  // No dependencies
 
-var regExp = /^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/;
+var regExp = /^\d{8}$/;       // 8 digits
 
 module.directive('cbaStaffNumber', function () {                  // Called once when the first instance of the directive is parsed.
                                                                   // Injection is possible, e.g. pass $http, $timeout, etc
     return {
         require: 'ngModel',                                       // Only applied if the "ng-model" attribute/directive is applied and
                                                                   // inject its controller as the fourth argument to the link function.
-        link: function (scope, element, attributes, controller) { // No injection. Called once, not once each time the value is changed.
-            element.bind("blur", function() {
+        link: function (scope, element, attributes, controller) { // No injection. Called once only , not once each time the value is 
+                                                                  // changed.
+            element.bind("blur", function() {                     // "blur" event = lost focus
                 controller.$setValidity(
                     'cbaStaffNumber',                             // Value to set on $error object
                     regExp.test(element[0].value));               // True/false state
-                scope.$apply();
+                scope.$apply();                                   // Force an update.
             });
         }
     };
 });
-
-// Controller
-function ValidationController($scope) {
-    // Do nothing
-}
