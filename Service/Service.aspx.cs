3a49655a5a9a7985d1c9c5810a51027a1a7b147d
Anthony Langsworth <anthony.langsworth@gmail.com>
2013-09-29 21:49:19 +1000

    Web method returning serialized object.
    * Removed Allow* from web.config.

--- Service/Service.aspx.cs
+++ Service/Service.aspx.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
 using System.Web;
 using System.Web.Script.Services;
 using System.Web.Services;
@@ -9,10 +13,18 @@ using System.Web.UI.WebControls;
 
 namespace AngularTraining.Service
 {
+    /// <summary>
+    /// Test DTO.
+    /// </summary>
+    public class Forecast
+    {
+        public int temp;
+        public string weather;
+    }
+
     public partial class Service : System.Web.UI.Page
     {
         [WebMethod()]
-        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
         public static string GetWeather()
         {
             // TODO: Check CSRF token
@@ -20,7 +32,33 @@ namespace AngularTraining.Service
             // NOTE: 
             // 1. Do not use single quotes
             // 2. Ensure all member names are quoted
-            return "{ \"temp\": 26, \"weather\": \"sunny\" }";
+
+            DataContractJsonSerializer dataContractJsonSerializer =
+                new DataContractJsonSerializer(typeof(Forecast));
+            Forecast forecast = new Forecast()
+            {
+                temp = 22,
+                weather = "Sunny"
+            };
+            string result;
+
+            // Wait 1 second
+            Thread.CurrentThread.Sleep(1000);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Serialize
+                dataContractJsonSerializer.WriteObject(memoryStream, forecast);
+                memoryStream.Flush();
+
+                // Get as a string
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                using (StreamReader streamReader = new StreamReader(memoryStream))
+                {
+                    result = streamReader.ReadToEnd();
+                }
+            }
+            return result;
         }
     }
 }
